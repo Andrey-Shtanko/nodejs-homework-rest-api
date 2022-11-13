601f552044e26b9e8033c7e624a3edc9c357223a
Andrey-Shtanko <shtankoandrew90@gmail.com>
2022-11-13 13:11:58 +0200

    add-some-routers

--- models/contacts.js
+++ models/contacts.js
@@ -1,14 +1,69 @@
-// const fs = require('fs/promises')
+const fs = require("fs/promises");
+const path = require("path");
+const contactsPath = path.resolve("models/contacts.json");
 
-const listContacts = async () => {}
+const listContacts = async () => {
+  try {
+    const response = await fs.readFile(contactsPath);
+    const contacts = JSON.parse(response);
+    return contacts;
+  } catch (error) {
+    console.log(error);
+  }
+};
 
-const getContactById = async (contactId) => {}
+const getContactById = async (contactId) => {
+  try {
+    const contacts = await listContacts();
+    const findedContact = contacts.find((contact) => contact.id === contactId);
+    if (!findedContact) {
+      return null;
+    }
+    return findedContact;
+  } catch (error) {
+    console.log(error);
+  }
+};
 
-const removeContact = async (contactId) => {}
+const removeContact = async (contactId) => {
+  try {
+    const contacts = await listContacts();
+    const deletedContactIndex = contacts.findIndex(
+      (contact) => contact.id === contactId
+    );
 
-const addContact = async (body) => {}
+    if (deletedContactIndex === -1) {
+      return null;
+    }
+    const deletedContact = contacts.splice(deletedContactIndex, 1);
 
-const updateContact = async (contactId, body) => {}
+    await fs.writeFile(contactsPath, JSON.stringify(contacts));
+    return deletedContact;
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+const addContact = async (body) => {
+  try {
+    const contacts = await listContacts();
+    const id = contacts.length + 1;
+    const newContact = {
+      id: `${id}`,
+      ...body,
+      // name,
+      // email,
+      // phone: `${phone}`,
+    };
+    const newContacts = [...contacts, newContact];
+    await fs.writeFile(contactsPath, JSON.stringify(newContacts));
+    return newContact;
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+const updateContact = async (contactId, body) => {};
 
 module.exports = {
   listContacts,
@@ -16,4 +71,4 @@ module.exports = {
   removeContact,
   addContact,
   updateContact,
-}
+};
--- routes/api/contacts.js
+++ routes/api/contacts.js
@@ -1,25 +1,48 @@
-const express = require('express')
+const express = require("express");
+const contacts = require("../../models/contacts.js");
 
-const router = express.Router()
+const router = express.Router();
 
-router.get('/', async (req, res, next) => {
-  res.json({ message: 'template message' })
-})
+router.get("/", async (req, res, next) => {
+  const contactList = await contacts.listContacts();
+  res.json({
+    status: "success",
+    code: 200,
+    data: {
+      contactList,
+    },
+  });
+});
 
-router.get('/:contactId', async (req, res, next) => {
-  res.json({ message: 'template message' })
-})
+router.get("/:contactId", async (req, res, next) => {
+  const { contactId } = req.params;
+  const contact = await contacts.getContactById(contactId);
 
-router.post('/', async (req, res, next) => {
-  res.json({ message: 'template message' })
-})
+  res.json({
+    status: "success",
+    code: 200,
+    data: { contact },
+  });
+});
 
-router.delete('/:contactId', async (req, res, next) => {
-  res.json({ message: 'template message' })
-})
+router.post("/", async (req, res, next) => {
+  const body = req.body;
+  const newContact = await contacts.addContact(body);
+  res.status(201).json({
+    status: "success",
+    code: 201,
+    data: { newContact },
+  });
+});
 
-router.put('/:contactId', async (req, res, next) => {
-  res.json({ message: 'template message' })
-})
+router.delete("/:contactId", async (req, res, next) => {
+  const { contactId } = req.params;
+  const deletedContact = await contacts.removeContact(contactId);
+  res.status(204).json(deletedContact);
+});
 
-module.exports = router
+router.put("/:contactId", async (req, res, next) => {
+  res.json({ message: "template message" });
+});
+
+module.exports = router;
